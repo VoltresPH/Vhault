46206a54f9602bc7bd4e6ba19252d30068c53248
Your Name <your.email@example.com>
2025-06-25 14:52:40 +0800

    base

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    // check if theres active session (for auth)
+    // if none, redirect to login
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vhault - Haul your files. Vault your world.</title>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,16 @@
+<?php
+    // check if theres active session (for auth)
+    // if yes, redirect to home
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up - Vhault</title>
+</head>
+<body>
+    
+</body>
+</html>
