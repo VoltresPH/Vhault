70ab75df1fe3900e7d293ec31a5598ae2f521fc9
fwancis9 <marcfrancislargo08@gmail.com>
2025-07-06 17:33:49 +0800

    ""

--- download.php
+++ download.php
@@ -14,6 +14,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
     
     <!-- custom css -->
+    <link rel="stylesheet" href="assets/global.css">
     <link rel="stylesheet" href="assets/download.css">
 </head>
 <body>
